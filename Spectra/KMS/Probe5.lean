c4801b348cf018dc2942ec2c5aeaf39fceaa33d7
adambornemann-glitch <adambornemann@tenkai-q.com>
2026-06-15 10:30:19 -0400

    house cleaning

--- Spectra/KMS/Probe5.lean
+++
@@ -1,7 +0,0 @@
-import Spectra.KMS.AnalyticElements
-
-open Complex Set Filter Topology MeasureTheory
-open Spectra.KMS
-
-example (t : ℝ) : ‖(t : ℂ)‖ = |t| := by rw [Complex.norm_real]
-example (t : ℝ) : ‖(t : ℂ)‖ = |t| := by rw [RCLike.norm_ofReal]
